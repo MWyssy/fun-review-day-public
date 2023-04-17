8585c1545e649e15f8aa40d8710b5a0448a5bc7c
MWyssy <mike@wyss.co.uk>
2023-04-17 12:15:07 +0100

    add Recursion tests and solutions

--- __tests__/4-recursion.test.js
+++ __tests__/4-recursion.test.js
@@ -4,6 +4,121 @@ const {
   flat,
 } = require("../sections/4-recursion.js");
 
-describe("function name here...", () => {
-  test("first test here...", () => {});
-});
+describe('deepEntries tests', () => {
+    test('Returns an array', () => {
+      //Arrange
+      const input = {};
+      const expectedOutput = [];
+      //Act
+      const output = deepEntries(input);
+      //Assert
+      expect(output).toEqual(expectedOutput);
+    });
+    test('When passed one non-nested object with a single key-value pair, it returns a nested array containing the key-value pair', () => {
+      //Arrange
+      const input = { name: "Sam" };
+      const expectedOutput = [['name', "Sam"]];
+      //Act
+      const output = deepEntries(input);
+      //Assert
+      expect(output).toEqual(expectedOutput);
+    });
+    test('When passed one non-nested object with mutliple key-value pair, it returns a nested array containing the key-value pairs', () => {
+      //Arrange
+      const input = {
+        name: "Sam", 
+        favBook: "Blood Meridian"
+      };
+      const expectedOutput = [
+        ['name', "Sam"], 
+        ['favBook', 'Blood Meridian']
+      ];
+      //Act
+      const output = deepEntries(input);
+      //Assert
+      expect(output).toEqual(expectedOutput);
+    });
+    test('When passed one one layer nested object with mutliple key-value pairs, it returns a nested array containing the key-value pairs nested as deeply as the original object', () => {
+      //Arrange
+      const input = {
+        name: "Sam", 
+        favBook: "Blood Meridian",
+        pets: {
+          name: 'Charlie',
+        }
+      };
+      const expectedOutput = [
+        ['name', "Sam"], 
+        ['favBook', 'Blood Meridian'],
+        ['pets', [['name', 'Charlie']]]
+      ];
+      //Act
+      const output = deepEntries(input);
+      //Assert
+      expect(output).toEqual(expectedOutput);
+    });
+    test('When passed one multi-layered nested object with mutliple key-value pairs, it returns a nested array containing the key-value pairs nested as deeply as the original object', () => {
+      //Arrange
+      const input = {
+        name: "Sam",
+        favBook: { 
+          title: "Blood Meridian", 
+          author: { 
+            name: "Cormac McCarthy" 
+          } 
+        },
+        pets: {
+          name: 'Charlie',
+        }
+      };
+      const expectedOutput = [
+        ["name","Sam"],
+        ["favBook", [
+          ["title","Blood Meridian"],
+          ["author", [
+            ["name","Cormac McCarthy"]
+          ]]]],
+        ['pets', [
+          ['name', 'Charlie']
+        ]]];
+      //Act
+      const output = deepEntries(input);
+      //Assert
+      expect(output).toEqual(expectedOutput);
+    });
+  });
+
+  describe('deeplyEquals tests', () => {
+    test('returns a boolean value', () => {
+      //Assert
+      expect(deeplyEquals()).toBe(false);
+    });
+    test('returns true if passed two values that are equal', () => {
+      //Assert
+      expect(deeplyEquals(1, 1)).toBe(true);
+    });
+    test('returns false if passed two values that are not equal', () => {
+      //Assert
+      expect(deeplyEquals(1, 2)).toBe(false);
+    });
+    test('When passed an array, it will evaluate the contents of the array', () => {
+      //Assert
+      expect(deeplyEquals([1], [1])).toBe(true);
+    });
+    test('When passed an object, it will evaluate the contents of the object', () => {
+      //Assert
+      expect(deeplyEquals({1: 1}, {1: 1})).toBe(true);
+    });
+    test('When passed a multi-layered array, it will evaluate the contents of all of the layers of the array', () => {
+      //Assert
+      expect(deeplyEquals([1, [2, [3]]], [1, [2, [3]]])).toBe(true);
+    });
+    test('When passed a multi-layered object, it will evaluate the contents of all of the layers of the object', () => {
+      //Assert
+      expect(deeplyEquals({1: {2: {3: 3}}}, {1: {2: {3: 3}}})).toBe(true);
+    });
+    test('When passed a multi-type array/object, it will evaluate the contents of all of the layers of the arrays/objects', () => {
+      //Assert
+      expect(deeplyEquals([ 'Numbers', {1: {2: {3: 3}}}, [4, [5]]], [ 'Numbers', {1: {2: {3: 3}}}, [4, [5]]])).toBe(true);
+    });
+  });
--- sections/4-recursion.js
+++ sections/4-recursion.js
@@ -1,7 +1,48 @@
 
-const deepEntries = () => { };
+const deepEntries = (object) => {
+    const result = [];
+    if (Object.keys(object).length === 0) return result;
 
-const deeplyEquals = () => { };
+    for (let i = 0; i < Object.keys(object).length; i++) {
+        const keyType = object[Object.keys(object)[i]]
+        if (typeof keyType === 'object') {
+            result.push([Object.keys(object)[i], deepEntries(keyType)]);
+        } else {
+            result.push([Object.keys(object)[i], Object.values(object)[i]]);
+        };
+    };
+    return result;
+ };
+
+const deeplyEquals = (a, b) => {
+    if (!a && !b) return false;
+    if (a === b ) return true;
+    
+    //Check if Array
+    if (Array.isArray(a) && Array.isArray(b)) {
+        for (let i = 0; i < a.length; i++) {
+            if (Array.isArray(a[i])) {
+                return deeplyEquals(a[i], b[i]);
+            };
+        };
+        return a.every((value, index) => value === b[index])
+    };
+
+    //Check if Object
+    if (typeof a === 'object' && typeof b === 'object') {
+        for (let key in a) {
+            if (typeof a[key] === 'object') {
+                return deeplyEquals(a[key], b[key]);
+            };
+        };
+        return Object.keys(a).every((value, index) => value === Object.keys(b)[index])
+            && Object.values(a).every((value, index) => value === Object.values(b)[index])
+    };
+
+
+
+    return false;
+ };
 
 const flat = () => { };
 

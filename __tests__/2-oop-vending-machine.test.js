ffccea4efd0f8fcfa30026ab92a375ed868525ab
MWyssy <mike@wyss.co.uk>
2023-04-17 10:28:04 +0100

    add OOP-Vending-Machine tests and solution

--- __tests__/2-oop-vending-machine.test.js
+++ __tests__/2-oop-vending-machine.test.js
@@ -1,5 +1,92 @@
 const VendingMachine = require("../sections/2-oop-vending-machine.js");
 
-describe("function name here...", () => {
-  test("first test here...", () => {});
+describe('VendingMachine tests', () => {
+  describe('VedingMachine Properties', () => {
+    test('It must have a credit property, which will be a number representing amount of pence, starting at 0.', () => {
+      //Arrange
+      const testVendingMachine = new VendingMachine();
+      //Assert
+      expect(testVendingMachine.credit).toBe(0);
+    });
+    test('It must have a stock property, an object representing the rows of items in the machine. Individual positions in the machine can then be referenced by the row, either "A", "B" or "C" e.g.', () => {
+      //Arrange
+      const testVendingMachine = new VendingMachine();
+      const output = {
+        'A': {},
+        'B': {},
+        'C': {},
+        'D': {},
+        'E': {},
+        'F': {}
+      }
+      //Assert
+      expect(testVendingMachine.stock).toEqual(output);
+    });
+  });
+  describe('VendingMachine Methods', () => {
+    test('It must have an addStock method which will add new stock to the vending machine at the correct position.', () => {
+      //Arrange
+      const testVendingMachine = new VendingMachine();
+      const marsBars = { name: "marsBar", price: 50, quantity: 6 }
+      const output = {
+        'A': marsBars,
+        'B': {},
+        'C': {},
+        'D': {},
+        'E': {},
+        'F': {}
+      };
+      //Act
+      testVendingMachine.addStock(marsBars, 'A');
+      //Assert
+      expect(testVendingMachine.stock).toEqual(output);
+    });
+    test('It must have an addCredit method which will update the machine credit.', () => {
+      //Arrange
+      const testVendingMachine = new VendingMachine();
+      //Act
+      testVendingMachine.addCredit(50);
+      //Assert
+      expect(testVendingMachine.credit).toBe(50);
+      //Act
+      testVendingMachine.addCredit(20);
+      //Assert
+      expect(testVendingMachine.credit).toBe(70);
+    });
+    test('It must have a purchaseItem method which will stock name, if there is sufficient credit.', () => {
+      //Arrange
+      const testVendingMachine = new VendingMachine();
+      const marsBars = { name: "marsBar", price: 50, quantity: 6 };
+      const crisps = { name: "crisps", price: 30, quantity: 6 }
+      //Act
+      testVendingMachine.addStock(marsBars, 'A');
+      testVendingMachine.addStock(crisps, 'B')
+      testVendingMachine.addCredit(30);
+      //Assert
+      expect(testVendingMachine.purchaseItem("B")).toBe('crisps');
+      expect(testVendingMachine.purchaseItem("A")).toBe('Insufficient Credit!');
+    });
+    test('The purchaseItem method will decrease the stock of the item purchased', () => {
+      //Arrange
+      const testVendingMachine = new VendingMachine();
+      const marsBars = { name: "marsBar", price: 50, quantity: 6 };
+      //Act
+      testVendingMachine.addStock(marsBars, 'A');
+      testVendingMachine.addCredit(50);
+      testVendingMachine.purchaseItem('A');
+      //Assert
+      expect(testVendingMachine.stock["A"].quantity).toBe(5);
+    });
+    test('The purchaseItem method will decrease the credit of the vending machine by the price of the purcahsed item', () => {
+      //Arrange
+      const testVendingMachine = new VendingMachine();
+      const marsBars = { name: "marsBar", price: 50, quantity: 6 };
+      //Act
+      testVendingMachine.addStock(marsBars, 'A');
+      testVendingMachine.addCredit(50);
+      testVendingMachine.purchaseItem('A');
+      //Assert
+      expect(testVendingMachine.credit).toBe(0);
+    });
+  });
 });
--- sections/2-oop-vending-machine.js
+++ sections/2-oop-vending-machine.js
@@ -1,3 +1,43 @@
-class VendingMachine {}
+class VendingMachine {
+    #credit;
+    #stock;
+    constructor() {
+        this.#credit = 0;
+        this.#stock = {
+            'A': {},
+            'B': {},
+            'C': {},
+            'D': {},
+            'E': {},
+            'F': {} 
+        };
+    };
+
+    get credit() {
+        return this.#credit
+    };
+    get stock() {
+        return this.#stock;
+    };
+
+    //Methods
+    addStock(newStock, row) {
+        return this.#stock[row] = newStock;
+    };
+
+    addCredit(addedCredit) {
+        return this.#credit += addedCredit;
+    };
+
+    purchaseItem(rowOfItem) {
+        if (this.#credit >= this.#stock[rowOfItem].price) {
+            this.#stock[rowOfItem].quantity--;
+            this.#credit -= this.#stock[rowOfItem].price;
+            return this.#stock[rowOfItem].name;
+        } else {
+            return 'Insufficient Credit!';
+        };
+    };
+};
 
 module.exports = VendingMachine;

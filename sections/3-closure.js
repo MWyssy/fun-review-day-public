e6a5878d5efa1af1caaa904f18310141e0f2d97a
MWyssy <mike@wyss.co.uk>
2023-04-17 10:52:50 +0100

    add closure solutions

--- sections/3-closure.js
+++ sections/3-closure.js
@@ -1,5 +1,20 @@
-function generateMultiples() {}
+function generateMultiples(multNum) {
+    return function multiply(lengthOfList) {
+        const result = [];
+        if (lengthOfList < 1) return result;
 
-function secureFunc() {}
+        for (let i = 1; i <= lengthOfList; i++) {
+            result.push(i * multNum);
+        };
+        return result;
+    };
+}
+
+function secureFunc(passsword, func) {
+    return function testPassword(passwordAttempt, ...args) {
+        if (passwordAttempt === passsword) return func(...args);
+        return 'Sorry your password is incorrect!';
+    };
+};
 
 module.exports = { generateMultiples, secureFunc };
